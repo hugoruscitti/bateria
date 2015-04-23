9690e001431404fe6dc4e9bedaec1b25eefc2bd1
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-04-23 16:21:29 -0300

    Comenzando a incorporar nexus-ui.

---
+++ app/components/ui-matrix.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  iniciar: function() {
+
+    nx.onload = function() {
+      matrix.row = 1;
+      matrix.col = 8;
+      matrix.init();
+
+      //beatmatrix5.matrix = [ [1,0,0,0],[0,1,0,0],[0,0,1,0] ];
+      //beatmatrix5.draw();
+    }
+
+  }.on('didInsertElement')
+
+});
