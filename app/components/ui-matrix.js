6a80c1cd3b44306a39f98a28e73491f8595775c2
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-04-25 20:53:14 -0300

    Pruebas generales.

--- app/components/ui-matrix.js
+++ app/components/ui-matrix.js
@@ -1,19 +1,49 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  element: null,
+  classNames: ['widget'],
+  tagName: 'div',
+  width: 200,
+  height: 100,
+  col: 5,
+  row: 2,
+  model: [],
+
   iniciar: function() {
+    var id = this.$()[0].getAttribute('id');
 
-    Ember.run.scheduleOnce('afterRender', this, function(){
-      nx.onload = function() {
-        matrix.row = 1;
-        matrix.col = 8;
-        matrix.init();
 
-        //beatmatrix5.matrix = [ [1,0,0,0],[0,1,0,0],[0,0,1,0] ];
-        //beatmatrix5.draw();
-      }
+    nx.sendsTo(function(data) {
+      console.log("A", data);
     });
 
-  }.on('didInsertElement')
+    var a = nx.add('matrix', {
+      w: this.get('width'),
+      h: this.get('height'),
+      name: 'volume' + id,
+      parent: id,
+    });
+
+    a.row = this.get('row');
+    a.col = this.get('col');
+    a.init();
+
+    this.set('element', a);
+
+    a.on("*", function(data) {
+      this.set("model", 'pepe');
+      //console.log(model);
+      //debugger;
+      //model[data.col] = data.level;
+      //console.log(model);
+    }.bind(this));
+
+  }.on('didInsertElement'),
+
+  eliminar: function() {
+    var el = this.get('element');
+    el.destroy();
+  }.on('willDestroyElement'),
 
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
+
 export default Ember.Route.extend({
+
+  model: function() {
+    return [1, 0, 0, 0, 0, 0, 0, 0];
+  }
 });
